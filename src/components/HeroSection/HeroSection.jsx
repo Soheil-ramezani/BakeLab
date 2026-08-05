6e0135a26323be199433690f591d08662c86d7f7
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-05 22:34:54 +0330

    HeroSection title added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,6 +1,11 @@
 import styles from './HeroSection.module.css'
 export default function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <section className={` flex flex-col mt-28 items-center `}>
+        <h1 className={`${styles.hero_title__h1} text-7xl text-center mx-[5%]`}>Master the Art, Taste the Craft</h1>
+        <h3 className={`${styles.hero_title__h3} text-3xl text-center mx-[5%]`}>Discover artisan breads crafted with passion and master baking techniques through our expert-led courses</h3>
+        <button className={`${styles.hero__button} w-fit mt-10 px-6 py-2`}><pre>See Menu</pre></button>
+        <p className={`${styles.hero__p} text-[14px] mt-4`}>* 10% off on first order</p>
+    </section>
   )
 }
