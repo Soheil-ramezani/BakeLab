0cff627d00745e53fb9f0a9306be05cfee6949b0
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-06 22:09:02 +0330

    Hero Section Items added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -6,6 +6,12 @@ export default function HeroSection() {
         <h3 className={`${styles.hero_title__h3} text-3xl text-center mx-[5%]`}>Discover artisan breads crafted with passion and master baking techniques through our expert-led courses</h3>
         <button className={`${styles.hero__button} w-fit mt-10 px-6 py-2`}><pre>See Menu</pre></button>
         <p className={`${styles.hero__p} text-[14px] mt-4`}>* 10% off on first order</p>
+        {/* pictures */}
+        <div className={`w-full grid grid-cols-[repeat(auto-fit,minmax(370px,1fr))] items-center justify-items-center gap-3  `}>
+          <img className={`${styles.hero_Pics__img}`} src="/images/JPG/greenSweet.jpg" alt="greenSweet"/>
+          <img className={`${styles.hero_Pics__img} xl:mt-[30%]`} src="/images/JPG/PinkSweet.jpg" alt="greenSweet"/>
+          <img className={`${styles.hero_Pics__img}`} src="/images/JPG/yellowSweets.jpg" alt="greenSweet"/>
+        </div>
     </section>
   )
 }
