582848d693ed8ca966936f1b50702928a3276e22
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-18 22:16:57 +0330

    OffersDetails became to a independent data file

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,14 +1,8 @@
+import { useState } from 'react'
 import styles from './Offers.module.css'
-
+import OffersDetail from '/src/Data/OffersGroupsPictures'
 export default function Offers() {
-  const OffersList = [
-    { id: 1, img: '/images/JPG/bread.jpg', title: 'Bread' },
-    { id: 2, img: '/public/images/JPG/cookie.jpg', title: 'Cookies' },
-    { id: 3, img: '/public/images/JPG/macron.jpg', title: 'Macron' },
-    { id: 4, img: '/public/images/JPG/pretzel.jpg', title: 'Pretzel' },
-    { id: 5, img: '/public/images/JPG/cupcake.jpg', title: 'CupCakes' },
-    { id: 6, img: '/public/images/JPG/cake.jpg', title: 'Cakes' },
-  ]
+  const [OffersList] =useState(OffersDetail) 
   return (
     <section className={`py-[10%] flex flex-col items-center `}>
       <h2 className={`${styles.offers_title__h2} font-bold`}>What we offer</h2>
