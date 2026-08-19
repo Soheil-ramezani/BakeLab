fc8eddbebe02c91955457a40cdaf3f593761d1ce
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-19 09:12:00 +0330

    image urls are corrected

--- src/Data/OffersGroupsPictures.js
+++ src/Data/OffersGroupsPictures.js
@@ -1,10 +1,10 @@
 const OffersDetail=[
     { id: 1, img: '/images/JPG/bread.jpg', title: 'Bread' },
-    { id: 2, img: '/public/images/JPG/cookie.jpg', title: 'Cookies' },
-    { id: 3, img: '/public/images/JPG/macron.jpg', title: 'Macron' },
-    { id: 4, img: '/public/images/JPG/pretzel.jpg', title: 'Pretzel' },
-    { id: 5, img: '/public/images/JPG/cupcake.jpg', title: 'CupCakes' },
-    { id: 6, img: '/public/images/JPG/cake.jpg', title: 'Cakes' },
+    { id: 2, img: '/images/JPG/cookie.jpg', title: 'Cookies' },
+    { id: 3, img: '/images/JPG/macron.jpg', title: 'Macron' },
+    { id: 4, img: '/images/JPG/pretzel.jpg', title: 'Pretzel' },
+    { id: 5, img: '/images/JPG/cupcake.jpg', title: 'CupCakes' },
+    { id: 6, img: '/images/JPG/cake.jpg', title: 'Cakes' },
   ]
 
   export default OffersDetail
