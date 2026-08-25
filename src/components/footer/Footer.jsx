180b01ebb1e78c61514819c35e79755b7de0a66f
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-25 11:54:17 +0330

    footer contact logoLinks took animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,20 +22,20 @@ export default function Footer() {
           make sureeverybody forward.</p>
         <div className={` w-4/5 flex flex-row items-center justify-around`}>
           {/* Facebook */}
-          <a href="#" title="Follow us on Facebook " className={`bg-(--white) h-full  p-2 w-auto rounded-full`}  >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+          <a href="#" title="Follow us on Facebook " className={`${styles.footer_link__a} h-full   w-auto rounded-full`}  >
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${styles.footer_link__svg}`} width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
           </a>
           {/* X */}
-          <a href="#" title="Follow us on X" className={`bg-(--white) h-full  p-2 w-auto rounded-full`} >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+          <a href="#" title="Follow us on X" className={`${styles.footer_link__a} h-full   w-auto rounded-full`} >
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${styles.footer_link__svg}`} width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
           </a>
           {/* Instagram */}
-          <a href="#" title="Follow us on Instagram" className={`bg-(--white) h-full  p-2 w-auto rounded-full`} >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+          <a href="#" title="Follow us on Instagram" className={`${styles.footer_link__a} h-full   w-auto rounded-full`} >
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${styles.footer_link__svg}`} width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
           </a>
           {/* YouTube */}
-          <a href="#" title="Follow us on YouTube" className={`bg-(--white) h-full  p-2 w-auto rounded-full`} >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
+          <a href="#" title="Follow us on YouTube" className={`${styles.footer_link__a} h-full   w-auto rounded-full`} >
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${styles.footer_link__svg}`} width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#050706" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
           </a>
         </div>
       </div>
