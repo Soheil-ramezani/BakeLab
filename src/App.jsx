dd455c271dd23f01c99c60dcc3d39acf9a9bfc8e
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-08-31 18:39:28 +0330

    Footer and navbar move to main.jsx and CourseList component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import  { Route, Routes } from 'react-router'
 import Navbar from "./components/Navbar/Navbar"
-import HomePage from "./Pages/HomePage/HomePage"
 import Footer from './components/footer/Footer'
+import HomePage from "./Pages/HomePage/HomePage"
+import CousresListPage from './Pages/CousresListPage/CousresListPage'
 function App() {
   return (
     <>
+    <Navbar/>
      <Routes>
       <Route path="/Home" element={<HomePage/>} />
+      <Route path="/" element={<CousresListPage/>} />
     </Routes>  
-    </>
+    <Footer/>
+   </>
   )
 }
 
---
+++ src/Pages/CousresListPage/CousresListPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function  CousresListPage() {
+  return (
+    <div >CousresListPage</div>
+  )
+}
