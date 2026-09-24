1b421ab79a418e90955f2d124e74c785224532f0
Soheil.Ramezani <soheilramezanie@gmail.com>
2026-09-24 18:39:29 +0330

    course page became responsive

--- src/Pages/Course/Course.jsx
+++ src/Pages/Course/Course.jsx
@@ -1,3 +1,4 @@
+import styles from './course.module.css'
 export default function Course() {
     const course = {
         id: 1,
@@ -133,17 +134,17 @@ export default function Course() {
         <main>
             <section className="w-full px-[5%]">
                 {/* image & title */}
-                <div className="w-full flex flex-row items-center my-5 gap-5 " >
+                <div className="w-full flex flex-col lg:flex-row items-center my-5 gap-5 " >
                     {/* image */}
-                    <div className={`w-2/5 h-max`}>
+                    <div className={`w-4/5 lg:w-2/5 h-max`}>
                         <img src={course.image} alt={course.title} className={`rounded-[10px] h-max`} />
                     </div>
                     {/* title Texts */}
-                    <div className={`w-3/5 flex flex-col items-center justify-center`}>
-                        <h1 className={`font-[BeauRivage] text-5xl`} >{course.title}</h1>
+                    <div className={`w-4/5 lg:w-3/5 flex flex-col items-center justify-center`}>
+                        <h1 className={`font-[BeauRivage] text-center`} >{course.title}</h1>
                         <p className={`font-[BeauRivage] text-3xl`}> {course.slug} </p>
                         {/* sign box */}
-                        <div className={` w-full mt-[2%]  bg-(--cream) flex flex-row items-center justify-around py-2.5 rounded-[10px]`}>
+                        <div className={`${styles.signBox__div} w-full mt-[2%]  bg-(--cream)  flex-row items-center justify-around py-2.5 rounded-[10px]`}>
                             {/* creator */}
                             <div className={`flex flex-col items-center gap-1.5`}>
                                 <img src="/public/images/favIcons/icons8-chef-70.png" alt="external-Cook-professional-avatars-smashingstocks-detailed-outline-smashing-stocks"></img>
@@ -167,28 +168,33 @@ export default function Course() {
                         </div>
                         {/* course description  */}
                         <p className={`mt-5 italic`}>{course.shortDescription}</p>
+                        <hr className={`my-[1.5%] w-full`} />
                         {/* price & enrol button */}
                         <div className={`w-full flex flex-row items-center justify-between mt-[1%]`}>
                             <div className={`h-full  flex flex-row text-nowrap gap-2.5 items-center justify-center `}>
-                             <p className="italic">Course Price: </p> 
-                             <p> {`\n`+`$`+course.price.original}</p>  
+                                <p className="italic">Course Price: </p>
+                                <p> {`\n` + `$` + course.price.original}</p>
                             </div>
                             <button className={`bg-(--cream) px-10 py-2.5 rounded-xl cursor-pointer self-end mr-[5%] `}> enrol</button>
                         </div>
-                        
+
                     </div>
                 </div>
                 {/* second Boxes */}
-                <div className={`flex flex-row items-start gap-[3%]`}>
+                <div className={`flex flex-col items-center md:flex-row md:items-start gap-[3vw]`}>
                     {/* left col */}
-                    <div className={`flex flex-col gap-[5vh]`}>
+                    <div className={`flex flex-col w-full md:w-2/5 gap-[3vw]`}>
                         {/* instructor */}
                         <div className={`flex flex-col w-full px-[5%] py-[5%]  text-left rounded-2xl bg-(--cream)`}>
                             <h3 className="italic">instructor</h3>
                             <hr />
-                            <p>{course.instructor.name}</p>
-                            <p>{course.instructor.expertise}</p>
-                            <p>rating:{course.instructor.rating}</p>
+                            <p> <span className=' mr-2.5'>name:</span>{course.instructor.name}</p>
+
+                            <p> <span className=' mr-2.5'>expertise:</span>{course.instructor.expertise}</p>
+                                
+                            <p><span className=' mr-2.5'> rating:</span>{course.instructor.rating}</p>
+                                
+                                
                         </div>
                         {/* time&level */}
                         <div className={`flex flex-col w-full px-[5%] py-[5%]  text-left rounded-2xl bg-(--cream)`}>
@@ -199,7 +205,6 @@ export default function Course() {
                         </div>
 
                     </div>
-
                     {/* right box */}
                     <div className={`flex flex-col w-fit px-[2.5%] py-[1.5%]  text-left rounded-2xl bg-(--cream)`}>
                         {/* description */}
@@ -222,7 +227,7 @@ export default function Course() {
                         )}
                         <hr className={`my-[1.5%]`} />
                         {/* prerequisites & outcomes */}
-                        <div className="flex flex-row items-start justify-around">
+                        <div className="flex flex-col sm:flex-row items-start justify-around">
                             {/* prerequisites */}
                             <div>
                                 <h3 className="italic text-xl underline">prerequisites</h3>
